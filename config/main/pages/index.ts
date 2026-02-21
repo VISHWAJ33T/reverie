90130f69a549c8453830224eb4a59ee41c1a0d50
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-02-21 19:38:44 +0530

    refactor: remove mainPageContactConfig and its export from index.ts for code cleanup

--- config/main/pages/index.ts
+++ config/main/pages/index.ts
@@ -1,4 +1,3 @@
 export { default as mainPageAboutConfig } from "./main-page-about-config";
-export { default as mainPageContactConfig } from "./main-page-contact-config";
 export { default as mainPagePolicyConfig } from "./main-page-policy-config";
 export { default as mainPageTermsConfig } from "./main-page-terms-config";
